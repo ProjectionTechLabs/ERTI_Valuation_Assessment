67b8ea3600e4c6313ba4b3ba0be3dd7eadb12c47
wintreetech <shubhammakwana@wintreetech.com>
2026-02-03 12:40:12 +0530

    done working till 3-02-26

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import AssessmentForm from "./components/AssessmentForm";
+
+function App() {
+	return (
+		<>
+			<div data-theme="light" style={{ colorScheme: "light" }} className="min-h-screen bg-white">
+				<AssessmentForm />
+			</div>
+		</>
+	);
+}
+
+export default App;
